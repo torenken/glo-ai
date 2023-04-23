5903fc8a5b988f190c86485aac353a2eeb9cdead
Thomas Renken <torenken@gmail.com>
2023-04-23 18:45:30 +0200

    little refactoring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
-<h1 class="text-3xl font-bold">
-  Welcome to GloAI
-</h1>
-<p>Visit <a href="https://github.com/torenken/glo-aiv">torenken/glo-ai</a> to read the documentation</p>
+<h1 class="text-3xl font-bold">Welcome to GloAI</h1>
+<p>
+	Visit <a href="https://github.com/torenken/glo-aiv">torenken/glo-ai</a> to read the documentation
+</p>
 
 <style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
 </style>
